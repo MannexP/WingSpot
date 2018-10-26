403095419836d08b65383fbdb95595f6c24b1716
Mannex Pimentel <Mannex@Mannexs-MacBook-Air.local>
2018-10-26 16:27:20 -0400

    routes all wacky

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,32 @@ const usersController = require('../controllers/users')
 const wingspotsController = require('../controllers/wingspots')
 const wingsController = require('../controllers/wings')
 
+//homepage
 router.get('/', applicationController.index)
 
-router.get('/', usersController.index)
-router.get('/new', usersController.new)
-router.post('/', usersController.create)
-router.get('/:id', usersController.show)
-router.get('/:id/edit', usersController.edit)
-router.put('/:id',usersController.update)
-router.delete('/:id', usersController.destroy)
+//All users page
+router.get('/users', usersController.index)
+//create a user form/submit
+router.get('/users/new', usersController.new)
+router.post('/users', usersController.create)
+//individual users page
+router.get('/users/:id', usersController.show)
+//update user
+router.get('/users/:id/edit', usersController.edit)
+router.put('/users/:id',usersController.update)
+//delete user account
+router.delete('/users/:id', usersController.destroy)
+
+// all users wingspots
+router.get('/users/:id/wingspot', wingspotsController.index)
+//create a new wingspot
+router.get('/users/:id/wingspot/new', wingspotsController.new)
+router.post('/users/:id/wingspots', wingspotsController.create)
+
+router.get('/users/:id', wingspotsController.show)
+router.get('/users/:id/edit', wingspotsController.edit)
+router.put('/users/:id',wingspotsController.update)
+router.delete('/users/:id', wingspotsController.destroy)
 
 
 
