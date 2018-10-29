3e8e5c33b77f88610eb958f82c88e4bc4ef1aa9a
Mannex Pimentel <Mannex@Mannexs-MacBook-Air.local>
2018-10-29 12:29:22 -0400

    mongodb env variables

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/WingSpot')
+mongoose.connect(process.env.MONGODB_URI)
 
 
 mongoose.connection.once('open', () => {
